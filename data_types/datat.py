d81cef4307d82ec142291c4ea48a7c6a32d3b8df
diegogc149 <gamarracruzdiego9@gmail.com>
2026-01-13 00:32:12 -0500

    Agrega tipos de datos

---
+++ data_types/datat.py
@@ -0,0 +1,30 @@
+interger_var = 10
+print(type(interger_var),interger_var)
+
+float_var = 4.50
+print(type(float_var),float_var)
+
+string_var = 'hello'
+print(type(string_var),string_var)
+
+boolean_var = True
+print(type(boolean_var),boolean_var)
+
+set_var = {7,6,10}
+print(type(set_var),set_var)
+
+dictionary = {'name': 'Diego','age':18}
+print(type(dictionary),dictionary)
+
+tuple = (3,8,1)
+print(type(tuple),tuple)
+
+range = range(10)
+print(type(range),range)
+
+list_var = [22, 'poto','XD']
+print(type(list_var),list_var)
+
+none_var = None
+print(type(none_var),none_var)
+
